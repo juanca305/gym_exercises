e89341f5f81446c1ca18321c36e902efdb0bb7be
Juan Carlos <jc.rdguez64@gmail.com>
2023-08-21 10:29:15 -0400

    ExerciseVideos_OK

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { fetchData, exerciseOptions } from '../utils/fetchData';
+import { fetchData, exerciseOptions, youtubeOptions } from '../utils/fetchData';
 
 import Detail from '../components/Detail';
 import SimilarExercises from '../components/SimilarExercises';
@@ -10,6 +10,7 @@ import ExerciseVideos from '../components/ExerciseVideos';
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -18,8 +19,11 @@ const ExerciseDetail = () => {
       const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
 
       const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
-      console.log(exerciseDetailData);
+      //console.log(exerciseDetailData);
       setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions);
+      setExerciseVideos(exerciseVideosData.contents);
     }
 
     fetchExercisesData();
@@ -29,7 +33,7 @@ const ExerciseDetail = () => {
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail}/>
-      <ExerciseVideos />
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name}/>
       <SimilarExercises />
     </Box>
   )
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -8,6 +8,15 @@ export const exerciseOptions = {
   }
 };
 
+export const youtubeOptions = {
+  method: 'GET',
+  //url: 'https://youtube-search-and-download.p.rapidapi.com/video/related',
+  headers: {
+    'X-RapidAPI-Key': '2f8f39983amshe5aed2d2a4a7544p1104edjsn164f9812224c',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }
+};
+
 export const fetchData = async (url, options) => {
     const response = await fetch (url, options);
     const data = await response.json();
