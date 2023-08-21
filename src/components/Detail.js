9506ff41a9014feb55a743b9872ea0954a30ae9a
Juan Carlos <jc.rdguez64@gmail.com>
2023-08-21 08:37:07 -0400

    Detail_OK

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -7,10 +7,26 @@ import EquipmentImage from '../assets/icons/equipment.png';
 
 const Detail = ({ exerciseDetail }) => {
     const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
-    console.log(gifUrl);
+    //console.log(gifUrl);
+
+    const extraDetail = [
+        {
+            icon: BodyPartImage,
+            name: bodyPart
+        },
+        {
+            icon: TargetImage,
+            name: target
+        },
+        {
+            icon: EquipmentImage,
+            name: equipment
+        }
+    ]
+
   return (
     <Stack gap='60px' sx={{flexDirection: {lg: 'row'}, p: '20px', alignItems: 'center'}}>
-        <img src={gifUrl} alt='name' loading='lazy' className='detail-image'/>;
+        <img src={gifUrl} alt='name' loading='lazy' className='detail-image'/>
         <Stack sx={{gap: {lg: '35px', xs: '20px'}}}>
             <Typography variant='h3'>
                 {name}
@@ -19,6 +35,16 @@ const Detail = ({ exerciseDetail }) => {
                 Exercises keep you strong. {name} {' '} is one of the best exercises to target your {target}.
                 It will help you improve your mood and energy.
             </Typography>
+            {extraDetail.map((item) => (
+                <Stack key={item.name} direction='row' gap='24px' alignItems='center'>
+                    <Button sx={{background: '#fff2db', borderRadius: '50%', width: '100px', height: '100px'}}>
+                        <img src={item.icon} alt={bodyPart} style={{width: '50px', height: '50px'}}/>
+                    </Button>
+                    <Typography variant='h5' textTransform='capitalize'>
+                        {item.name}
+                    </Typography>
+                </Stack>
+            ))}
         </Stack>
     </Stack>
   )
